b9fb26b635d00361d9c6038e853c0ed1cefa9b4f
Jan Stevens <janstevens007@gmail.com>
2014-08-10 13:56:14 +0200

    Remove old demo folder, fixed some merge mistakes

--- build/angular-growl.js
+++ build/angular-growl.js
@@ -132,7 +132,7 @@ angular.module('angular-growl').run([
   function ($templateCache) {
     'use strict';
     if ($templateCache.get('templates/growl/growl.html') === undefined) {
-      $templateCache.put('templates/growl/growl.html', '<div class="growl-container" ng-class="wrapperClasses()">' + '<div class="growl-item alert" ng-repeat="message in messages" ng-class="alertClasses(message)">' + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true" ng-click="deleteMessage(message)" ng-show="!message.disableCloseButton">&times;</button>' + '<button type="button" class="close" aria-hidden="true" ng-show="message.ttl > 0">{{message.countdown}}</button>' + '<h4 class="growl-title" ng-show="message.title" ng-bind="message.title"></h4>' + '<div class="growl-message" ng-bind-html="message.text"></div>' + '</div>' + '</div>');
+      $templateCache.put('templates/growl/growl.html', '<div class="growl-container" ng-class="wrapperClasses()">' + '<div class="growl-item alert" ng-repeat="message in messages" ng-class="alertClasses(message)" ng-click="stopTimeoutClose(message)">' + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true" ng-click="deleteMessage(message)" ng-show="!message.disableCloseButton">&times;</button>' + '<button type="button" class="close" aria-hidden="true" ng-show="message.ttl > 0">{{message.countdown}}</button>' + '<h4 class="growl-title" ng-show="message.title" ng-bind="message.title"></h4>' + '<div class="growl-message" ng-bind-html="message.text"></div>' + '</div>' + '</div>');
     }
   }
 ]);
--- src/growlDirective.js
+++ src/growlDirective.js
@@ -147,7 +147,7 @@ angular.module("angular-growl").run(['$templateCache', function($templateCache) 
   if ($templateCache.get('templates/growl/growl.html') === undefined) {
     $templateCache.put("templates/growl/growl.html",
       '<div class="growl-container" ng-class="wrapperClasses()">' +
-        '<div class="growl-item alert" ng-repeat="message in messages" ng-class="alertClasses(message)">' +
+        '<div class="growl-item alert" ng-repeat="message in messages" ng-class="alertClasses(message)" ng-click="stopTimeoutClose(message)">' +
           '<button type="button" class="close" data-dismiss="alert" aria-hidden="true" ng-click="deleteMessage(message)" ng-show="!message.disableCloseButton">&times;</button>' +
           '<button type="button" class="close" aria-hidden="true" ng-show="message.ttl > 0">{{message.countdown}}</button>' +
           '<h4 class="growl-title" ng-show="message.title" ng-bind="message.title"></h4>' +
