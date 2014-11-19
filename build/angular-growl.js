3860a254efef7e5292f3e5c3c50fc861398dc1f9
Pieter <pieter.willaert@stackandheap.com>
2014-11-19 12:38:43 +0100

    cleanup: removed comments

--- build/angular-growl.js
+++ build/angular-growl.js
@@ -66,55 +66,24 @@ angular.module('angular-growl').directive('growl', [
 angular.module('angular-growl').provider('growl', function () {
   'use strict';
   var _ttl = null, _enableHtml = false, _messagesKey = 'messages', _messageTextKey = 'text', _messageSeverityKey = 'severity', _onlyUniqueMessages = true;
-  /**
-	 * set a global timeout (time to live) after which messages will be automatically closed
-	 *
-	 * @param ttl in seconds
-	 */
   this.globalTimeToLive = function (ttl) {
     _ttl = ttl;
   };
-  /**
-	 * set whether HTML in message content should be escaped (default) or binded as-is
-	 *
-	 * @param {bool} enableHtml true to make all messages not escapes
-	 */
   this.globalEnableHtml = function (enableHtml) {
     _enableHtml = enableHtml;
   };
-  /**
-	 * sets the key in $http response the serverMessagesInterecptor is looking for server-sent messages, value of key
-	 * needs to be an array of objects
-	 *
-	 * @param {string} messagesKey default: messages
-	 */
   this.messagesKey = function (messagesKey) {
     _messagesKey = messagesKey;
   };
-  /**
-	 * sets the key in server sent messages the serverMessagesInterecptor is looking for text of message
-	 *
-	 * @param {string} messageTextKey default: text
-	 */
   this.messageTextKey = function (messageTextKey) {
     _messageTextKey = messageTextKey;
   };
-  /**
-	 * sets the key in server sent messages the serverMessagesInterecptor is looking for severity of message
-	 *
-	 * @param {string} messageSeverityKey default: severity
-	 */
   this.messageSeverityKey = function (messageSeverityKey) {
     _messageSeverityKey = messageSeverityKey;
   };
   this.onlyUniqueMessages = function (onlyUniqueMessages) {
     _onlyUniqueMessages = onlyUniqueMessages;
   };
-  /**
-	 * $http interceptor that can be added to array of $http interceptors during config phase of application
-	 * via $httpProvider.responseInterceptors.push(...)
-	 *
-	 */
   this.serverMessagesInterceptor = [
     '$q',
     'growl',
@@ -162,47 +131,18 @@ angular.module('angular-growl').provider('growl', function () {
         };
         broadcastMessage(message);
       }
-      /**
-		 * add one warn message with bootstrap class: alert
-		 *
-		 * @param {string} text
-		 * @param {{ttl: number}} config
-		 */
       function addWarnMessage(text, config) {
         sendMessage(text, config, 'warn');
       }
-      /**
-		 * add one error message with bootstrap classes: alert, alert-error
-		 *
-		 * @param {string} text
-		 * @param {{ttl: number}} config
-		 */
       function addErrorMessage(text, config) {
         sendMessage(text, config, 'error');
       }
-      /**
-		 * add one info message with bootstrap classes: alert, alert-info
-		 *
-		 * @param {string} text
-		 * @param {{ttl: number}} config
-		 */
       function addInfoMessage(text, config) {
         sendMessage(text, config, 'info');
       }
-      /**
-		 * add one success message with bootstrap classes: alert, alert-success
-		 *
-		 * @param {string} text
-		 * @param {{ttl: number}} config
-		 */
       function addSuccessMessage(text, config) {
         sendMessage(text, config, 'success');
       }
-      /**
-		 * add a indefinite number of messages that a backend server may have sent as a validation result
-		 *
-		 * @param {Array.<object>} messages
-		 */
       function addServerMessages(messages) {
         var i, message, severity, length;
         length = messages.length;
